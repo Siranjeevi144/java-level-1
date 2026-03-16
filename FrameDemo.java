2c4b53e6e1f2de431dca0d7423730c8468cac62d
Siranjeevi144 <siranjeevivengadesan@gmail.com>
2026-03-16 12:47:32 +0530

    program1

---
+++ FrameDemo.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+ 
+class FrameDemo {
+
+public static void main(String arg[]) {
+
+Frame f = new Frame();
+
+f.setTitle("Siranjeevi-BCA");
+
+f.setSize(500, 400);
+
+f.setVisible(true);
+
+}
+
+}
